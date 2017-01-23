4e93b935fd003fff88ec10f0deef84bfaafe8f96
imrahamed <imrahamed@gmail.com>
2017-01-23 16:50:42 +0530

    Update register.service.ts

--- src/app/register/register.service.ts
+++ src/app/register/register.service.ts
@@ -11,7 +11,7 @@ export class UserService {
 
 
     create(user: User): Observable<boolean>{
-        return this.http.post('http://192.168.1.142:8080/register', user)
+        return this.http.post('http://127.0.0.1:3333/register', user)
 .map((response: Response) => {
                 // login successful if there's a jwt token in the response
                 let status = response.json() && response.json().status;
